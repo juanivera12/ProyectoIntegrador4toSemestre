1d530a013621212aca4b60829492a71888c54dc0
Horacio Quiles <quileshoxar@gmail.com>
2025-10-31 12:03:05 -0300

    agrego configuracion del backend de mercado pago

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,38 @@ const usersRoutes = require('./routes/users'); // Importa las nuevas rutas
 const app = express();
 const PORT = 3000;
 
+// SDK de Mercado Pago
+import { MercadoPagoConfig, Preference } from 'mercadopago';
+// Agrega credenciales
+const client = new MercadoPagoConfig({ accessToken: 'APP_USR-5297062915426978-090319-d6089e8ecdab5289c86405e84054f715-2670517922' });
+
+app.post('/create_preference', async (req, res) => {
+    const preference = new Preference(client);
+
+preference.create({
+  body: {
+    items: [
+      {
+        title: 'Mi producto',
+        quantity: 1,
+        unit_price: 2000
+      }
+    ],
+  }
+})
+.then(data => {
+    console.log(data);
+    // objeto data tiene la información de la preferencia creada
+    res.status(200).json({
+        preference_id: data.id,
+        preference_url: data.init_point,
+    });
+  })
+  .catch(() => {
+    res.status(500).json({ "error": "error creating preference"})
+  });
+});
+
 // Middleware: Permite que Express lea cuerpos de petición JSON (para POST y PUT)
 app.use(bodyParser.json());
 
